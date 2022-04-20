5994d710345579f400f12379c765f25b51a54ed0
aaridwann <aaridwann@gmail.com>
2022-04-21 00:54:33 +0700

    Vendor Controller find use Aggregation

--- test query.js
+++ test query.js
@@ -1,4 +1,26 @@
 // REGEX QUERY
 
-db.vendors.find({name:{$regex:/rudi/i},category:{$regex:/makeup/i}})
+db.vendors.find({ name: { $regex: /rudi/i }, category: { $regex: /makeup/i } })
 
+// Joint
+db.vendors.aggregate([
+    {
+        $lookup: {
+            from: 'schedules',
+            localField: 'vendorId',
+            foreignField: 'vendorId',
+            as: 'schedules'
+        }
+    },
+    { $match: { name: {$regex:/ /i} } },
+    { $project: { name: 1, 'schedules': '$schedules.scheduleList.eventDate' } },
+])
+// Joint 2
+db.vendors.aggregate([
+    { $match: { name: {} } },
+    { $group: { _id: '$name' } }
+])
+// distinct
+db.vendors.distinct('category')
+// count
+db.orders.count({ vendorName: 'rudi makeup', orderList })
