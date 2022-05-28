47a4815626c92494e4eb473cbc8ac6e479b60aad
Ridwan Firmansyah <aaridwann@gmail.com>
2022-05-28 16:01:12 +0700

    Finished add function

--- Controller/Function/Add Event/AddEventFunction.ts
+++ Controller/Function/Add Event/AddEventFunction.ts
@@ -36,21 +36,21 @@ export const AddEventFunction = async (req: RequestAddEventFunctionInterface, re
     // Validation data
     let data: any = await validationData(req.body)
     if (data?.state == false) {
-        return res.status(400).json(data.message)
-    } 
+        return res.status(400).json({ state: false, message: data.message })
+    }
 
     // Step 2
     // Validation Duplicate Event Function
     let validateDuplicateEvent = await ValidationDuplicateEvent(data?.eventName, req.user._id)
     if (validateDuplicateEvent.state === false) {
-        return res.status(400).json(validateDuplicateEvent.message)
+        return res.status(400).json({ state: false, message: validateDuplicateEvent.message })
     }
 
     // Step 3
     // Validation user is vendor or not and found
     let validatorUser = await validationUser(req.user._id)
     if (validatorUser.state == false) {
-        return res.status(400).json(validatorUser.message)
+        return res.status(400).json({ stat: false, message: validatorUser.message })
     }
 
     // Step 4
@@ -60,13 +60,12 @@ export const AddEventFunction = async (req: RequestAddEventFunctionInterface, re
         await insert.save()
         return res.status(201).json({ state: true, message: 'success create event' })
     } catch (error) {
-        return res.status(400).json(error)
+        return res.status(400).json({ state: false, message: error })
     }
 
 }
 
-// 
-// Error di validation data function
+// Child Function
 
 async function validationData(req: any): Promise<{ state: boolean, message: any } | Format> {
 
@@ -90,8 +89,15 @@ async function validationData(req: any): Promise<{ state: boolean, message: any 
     let date = (date: string): number => {
         let ml = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
         let month = ml.indexOf(date.split(' ')[1])
+        if (month == -1) {
+            return 0
+        }
         let data = new Date(parseInt(date.split(' ')[2]), month, parseInt(date.split(' ')[0])).getTime()
         return data
+    }
+
+    if (date(req.eventDate) == 0) {
+        return { state: false, message: 'check date month or year format' }
     }
 
     // Init data from body
@@ -106,7 +112,6 @@ async function validationData(req: any): Promise<{ state: boolean, message: any 
     return data
 }
 
-
 async function ValidationDuplicateEvent(eventName: string, userId: string) {
     let res = await eventDb.findOne({ userId: userId, 'event.eventName': eventName })
     if (res) {
@@ -115,7 +120,6 @@ async function ValidationDuplicateEvent(eventName: string, userId: string) {
         return { state: true }
     }
 }
-
 
 async function validationUser(idUser: string): Promise<{ state: boolean, message?: string }> {
 
