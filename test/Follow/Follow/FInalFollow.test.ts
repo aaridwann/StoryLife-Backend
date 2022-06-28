db25c8f20223040af44f8a9689e1a749c15232e5
Ridwan Firmansyah <aaridwann@gmail.com>
2022-06-28 23:22:58 +0700

    feature following complete

---
+++ test/Follow/Follow/FInalFollow.test.ts
@@ -0,0 +1,39 @@
+import { follow } from "../../../Controller/Function/Follow/Follow";
+import { Response } from 'express'
+require('dotenv').config()
+import mongoose from "mongoose"
+const url = process.env.DB_URL
+beforeAll(async () => {
+    await mongoose.connect(url)
+})
+afterAll(async () => {
+    await mongoose.connection.close()
+})
+
+
+describe('Follow Head unit testing', () => {
+
+    test('user id or id target not found', async () => {
+        let data = {
+            user: { _id: '' },
+            params: { id: '' }
+        }
+        expect(await follow(data)).toEqual({ state: false, message: 'id user or id params not found' })
+    })
+    test('already following', async () => {
+        let data = {
+            user: { _id: '62bb152a0d650fe80159d101' },
+            params: { id: 'abc' }
+        }
+        expect(await follow(data)).toEqual({ state: false, message: 'you already follow' })
+    })
+
+    test('Success Following', async () => {
+        let data = {
+            user: { _id: '62bb152a0d650fe80159d101' },
+            params: { id: '62bb15350d650fe80159d10d' }
+        }
+        expect(await follow(data)).toEqual({ state: false, message: 'you already follow' })
+    })
+
+})
