a0026efc62352fa33474c5be2863594de59df348
Ridwan Firmansyah <aaridwann@gmail.com>
2022-09-10 11:39:13 +0700

    delete compile

--- Controller/Service/Auth/RefreshToken.ts
+++ Controller/Service/Auth/RefreshToken.ts
@@ -1,3 +1,9 @@
-export const refreshToken = async () => {
+import { Response } from "express";
+const bcrypt = require("bcrypt")
+
+export const refreshToken = async (req: { cookies: string }, res: Response) => {
+    console.log(req.cookies);
+    // const decrypt = bcrypt
+    return res.status(200).json(req.cookies)
 
 }
