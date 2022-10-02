2cff4a254fcac83dcbd4fdc9dd1f16befd1acf61
Ridwan Firmansyah <aaridwann@gmail.com>
2022-10-02 17:07:28 +0700

    remove xtension index.ts

--- Routes/Users/UsersRoute.ts
+++ Routes/Users/UsersRoute.ts
@@ -1,5 +1,5 @@
 import { getUser } from "../../Controller/Service/Users/UsersController"
-import UploadTest from "../../Controller/Service/Upload/Test/index"
+import UploadTest from "../../Controller/Service/Upload/Test/"
 import { uploadSingle } from "../../Utils/Multer"
 const { verify } = require('../../MiddleWare/TokenMiddleware')
 const express = require('express')
